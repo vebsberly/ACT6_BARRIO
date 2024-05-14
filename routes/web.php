8962f9bc0054b1d743565ccd5282448ac6e4d27e
vebsberly <vebarrio@my.cspc.edu.ph>
2024-05-14 10:40:44 +0800

    ACT6_BARRIO

---
+++ routes/web.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('posts.index');
+});
+
+Route::get('/posts/{post}', function ($post) {
+    return view('posts.show', [
+        'post' => $post
+        ]);
+});
